0a8a813d0ca816e075c6b909490df8652b5765af
azatulepbergenov <azamattulepbergenov@u.boisestate.edu>
2015-07-06 17:40:20 -0600

    This commit includes somewhat working version of csv file, though, need more MORE work.

--- fiberfit/fiberfit_control.py
+++ fiberfit/fiberfit_control.py
@@ -3,6 +3,7 @@
 """This is a control part of the GUI application"""
 
 import sys
+import csv
 import matplotlib
 matplotlib.use("Qt5Agg") ## forces to use Qt5Agg so that Backends work
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -12,12 +13,13 @@ from PyQt5 import QtWidgets
 from PyQt5.Qt import*
 from PyQt5.QtWidgets import QFileDialog
 from fiberfit import img_model
-
+from pandas import DataFrame
+from pandas import *
 class fft_mainWindow(fiberfit_GUI.Ui_MainWindow, QtWidgets.QMainWindow):
-
     def __init__(self, Parent = None):
         super(fft_mainWindow, self).__init__()
         self.imgList = []
+        self.dataList = []
         self.setupUi(self)
         self.numImages = 0
         self.currentIndex = 0
@@ -48,14 +50,14 @@ class fft_mainWindow(fiberfit_GUI.Ui_MainWindow, QtWidgets.QMainWindow):
             # of this first element
             th, k, fig = computerVision_BP.process_image(self.filename[0][i])
             #creates a class imgModel and appends to the list of all images
-            self.imgList.append(img_model.imgModel(th,k, fig)) # works!;-)
+            self.imgList.append(img_model.imgModel(self.filename[0][i],th,k, fig)) # works!;-)
         if self.isStarted:
             self.gridLayout.removeWidget(self.canvas)
         self.canvas = FigureCanvas((self.imgList[self.currentIndex].getFig())) # works!;-)
         self.gridLayout.addWidget(self.canvas)
         self.isStarted = True
         self.figureFrame.show()
-
+        self.numImages += 1
 
     def start(self):
         # sets the current index to the numImages before numImages get updated.
@@ -64,7 +66,12 @@ class fft_mainWindow(fiberfit_GUI.Ui_MainWindow, QtWidgets.QMainWindow):
         self.currentIndex = self.numImages
         self.processImages()
         self.setupLabels(self.currentIndex)
-        self.numImages += 1
+        for i in range (0, self.numImages):
+            self.dataList.append([str(self.imgList[i].getName()), str(self.imgList[i].getTh()), str(self.imgList[i].getK())])
+        with open('test.csv', 'w', newline='') as fp:
+            a = csv.writer(fp, delimiter=',')
+            a.writerows(self.dataList)
+        #self.numImages += 1
 
     def setupLabels(self, num):
         self.kLabel.setText("k = " + str(round(self.imgList[num].getK(),2)))
--- fiberfit/img_model.py
+++ fiberfit/img_model.py
@@ -3,15 +3,22 @@ Class representing a an imgage model, encapsulating th and k.
 """
 class imgModel:
     count = 0 # for all classes count starts from 0
-    def __init__(self, th = None, k = None, figure = None):
+    def __init__(self, filename = None, th = None, k = None, figure = None):
+        self.filename = filename
         self.th = th
         self.k = k
         self.figure = figure
     def __repr__(self):
         return "image" + str(self.count)
+
     def getTh(self):
         return self.th
+
     def getK(self):
         return self.k
+
     def getFig(self):
         return self.figure
+
+    def getName(self):
+        return self.filename
