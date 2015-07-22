09eba9f7747ee85589a4d5ed4a0de45df219694c
azatulepbergenov <azamattulepbergenov@u.boisestate.edu>
2015-07-22 17:42:15 -0600

    This commiot includes what Jaremy helped me with in the evening. Signals and Slots.

--- fiberfit/fiberfit_control.py
+++ fiberfit/fiberfit_control.py
@@ -20,9 +20,59 @@ from PyQt5.QtCore import QFileInfo  # In order to get a pathname to a file.
 from fiberfit import img_model
 from orderedset import OrderedSet
 import base64
+from PyQt5.QtPrintSupport import QPrinter
+from PyQt5.QtGui import QTextDocument
+from PyQt5.QtWebKitWidgets import QWebView
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, Qt
+from PyQt5.QtWidgets import QPushButton, QDialogButtonBox, QVBoxLayout, QDialog
+
+
+class MyDialog(QDialog):
+    def __init__(self, parent=None):
+        super(MyDialog, self).__init__(parent)
+
+        self.buttonBox = QDialogButtonBox(self)
+        self.buttonBox.setOrientation(Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+
+        self.textBrowser = QWebView(self)
+        self.textBrowser.setHtml("This is a QTextBrowser!")
+
+        self.verticalLayout = QVBoxLayout(self)
+        self.verticalLayout.addWidget(self.textBrowser)
+        self.verticalLayout.addWidget(self.buttonBox)
+
+
+    def createHtml(self, model):
+        html = """
+        <html> <link type="text/css" rel="stylesheet" href="ntm_style.css"> <body>
+        <p> Image Name: {name} </p> <p> mu: %s </p>
+        <p>k: {k} </p>
+        <table>
+            <tr>
+                <td> <img src = \"orgImg.png\"/> </td>
+                <td> <img src =\"logScl.png\"/> </td>
+            </tr>
+        </table>
+
+        """.format(name = model.filename.stem, k = model.k)
+        html += ("<html> <link type = \"text/css\" rel = \"stylesheet\" href = \"ntm_style.css\"> <body>")
+        html += ("<p> Image Name: %s </p> <p> mu: %s </p> <p>k: %s </p> <table> <tr> <td> <img src = \"orgImg.png\"/> </td> <td> <img src =\"logScl.png\"/> </td> </tr> </table>" %
+                 (model.filename.stem, model.th, model.k ))
+        html += ("</body> </html>")
+        print(html)
+        return html
+
+    @pyqtSlot(img_model.ImgModel)
+    def do_test(self, model):
+        print("Test?")
+        self.textBrowser.setHtml(self.createHtml(model))
+        self.show()
 
 class fft_mainWindow(fiberfit_GUI.Ui_MainWindow, QtWidgets.QMainWindow):
     results = open('test.csv', 'a')  # All instances would have this as a starter. Initialized later in code.
+    show_report = pyqtSignal(int)
+    make_report = pyqtSignal(img_model.ImgModel)
 
     """
     Initializes all instance variables a.k.a attributes of a class.
@@ -53,6 +103,30 @@ class fft_mainWindow(fiberfit_GUI.Ui_MainWindow, QtWidgets.QMainWindow):
         # sends off a signal containing string.
         # Conveniently the string will be the name of the file.
         self.selectImgBox.activated[str].connect(self.changeState)
+
+        #New button
+        self.pushButtonWindow = QPushButton(self)
+        self.pushButtonWindow.setText("Click Me!")
+        self.pushButtonWindow.clicked.connect(lambda i: self.show_report.emit(self.currentIndex))
+
+        self.gridLayout.addWidget(self.pushButtonWindow)
+
+        def userLog(int):
+            print("User requested reported for image: {}".format(int))
+
+        self.show_report.connect(userLog)
+
+        #self.layout = QtGui.QHBoxLayout(self)
+        #self.layout.addWidget(self.pushButtonWindow)
+        self.dialogTextBrowser = MyDialog(self)
+
+        def do_show_report(index):
+            print('Shows report')
+            self.make_report.emit(self.imgList[self.currentIndex])
+            #self.dialogTextB3rowser.show()
+
+        self.show_report.connect(do_show_report)
+        self.make_report.connect(self.dialogTextBrowser.do_test)
 
     """
     Clears out canvas.
@@ -226,8 +300,8 @@ class fft_mainWindow(fiberfit_GUI.Ui_MainWindow, QtWidgets.QMainWindow):
     # TODO: Need to add saving of PDF option.
     def export(self):
         for i in range(self.csvIndex, len(self.imgList)):
-            self.decodeFigures(i)
-
+            self.decodeFigures(1)
+            self.printOutput(1)
             self.dataList.append(
                 [self.imgList.__getitem__(i).filename.stem, self.imgList.__getitem__(i).th, self.imgList.__getitem__(i).k,
                  self.imgList.__getitem__(i).timeStamp])
@@ -243,9 +317,20 @@ class fft_mainWindow(fiberfit_GUI.Ui_MainWindow, QtWidgets.QMainWindow):
     Note, O(n) because of performing a search for image.
     """
 
-    def printOutput(self):
-        html = u""
-        html += 
+    def printOutput(self, i):
+        html = ""
+        html += ("<html> <link type = \"text/css\" rel = \"stylesheet\" href = \"ntm_style.css\"> <body>")
+        html += ("<p> Image Name: %s </p> <p> mu: %s </p> <p>k: %s </p> <table> <tr> <td> <img src = \"orgImg.png\"/> </td> <td> <img src =\"logScl.png\"/> </td> </tr> </table>" % (self.imgList.__getitem__(i).filename.stem, self.imgList.__getitem__(i).th, self.imgList.__getitem__(i).k ))
+        html += ("</body> </html>")
+
+    def printerSetup(self):
+        printer = QPrinter()
+        printer.setPageSize(QPrinter.Letter)
+        printer.setOutputFormat(QPrinter.PdfFormat)
+        printer.setOutputFileName('ResultTable.pdf')
+        document = QTextDocument()
+        document.setHtml(html)
+        document.print_(printer)
 
     def changeState(self, filename):
         # find img
@@ -270,10 +355,15 @@ class fft_mainWindow(fiberfit_GUI.Ui_MainWindow, QtWidgets.QMainWindow):
         angDistDecoded = open("angDist.png", "wb")
         angDistDecoded.write(base64.b64decode(self.imgList.__getitem__(i).angDistEncoded))
 
-"""
-Enters an event-loop.
-"""
-app = QtWidgets.QApplication(sys.argv)
-fft_app = fft_mainWindow()
-fft_app.show()
-sys.exit(app.exec_())
+
+def main():
+    """
+    Enters an event-loop.
+    """
+    app = QtWidgets.QApplication(sys.argv)
+    fft_app = fft_mainWindow()
+    fft_app.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
