127ce72a8852f764959d38fae671743cd22168b5
azatulepbergenov <azamattulepbergenov@u.boisestate.edu>
2015-08-02 13:57:58 -0600

    This is where I am at the beginning of major refactoring.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='fiberfit',
+    version='',
+    packages=['fiberfit'],
+    url='',
+    license='',
+    author='azatulepbergenov',
+    author_email='',
+    description=''
+)
