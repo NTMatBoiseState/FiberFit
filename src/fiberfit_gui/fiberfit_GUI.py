c6774adb86d994a99bb7a9df0f713b9791ff9b23
atulep <aza.tulepber@gmail.com>
2016-06-19 15:21:26 -0600

    Commented out the fiberfit_GUI file.

--- src/fiberfit_gui/fiberfit_GUI.py
+++ src/fiberfit_gui/fiberfit_GUI.py
@@ -1,11 +1,22 @@
-#!/usr/local/bin/python3
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 import os
 
+
 class Ui_MainWindow(object):
+    """
+    Defines the view of the main FiberFit UI (the first thing you see when you run FiberFit
+    """
     def setupUi(self, MainWindow, heigth, width):
+        """
+        Sets up all of the GUI components of the main FiberFit UI.
+        Created by PyQt5 UI code generator 5.4
+        :param MainWindow: instance of fiberfit.fft_mainWindow
+        :param heigth: height of the screen
+        :param width: width of the screen
+        :return: none
+
+        """
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(0.3*int(width.__str__()), 0.1*int(heigth.__str__()))
         MainWindow.setMinimumSize(QtCore.QSize(0, 0))
@@ -32,37 +43,28 @@ class Ui_MainWindow(object):
         self.selectImgBox.setMaximumSize(QtCore.QSize(170, 16777215))
         self.selectImgBox.setObjectName("selectImgBox")
         self.midGrid.addWidget(self.selectImgBox, 0, 0, 1, 1)
-
-          #  Progress bar
+        # Progress bar
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding,
                                            QtWidgets.QSizePolicy.MinimumExpanding)
-
         self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
-
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.progressBar.sizePolicy().hasHeightForWidth())
-
         self.progressBar.setSizePolicy(sizePolicy)
         self.progressBar.setMinimum(0)
         self.progressBar.setMinimumSize(QtCore.QSize(50, 0))
         self.progressBar.setMaximumSize(QtCore.QSize(50, 20))
-
         self.progressBar.setProperty("value", 0)
         self.progressBar.setObjectName("progressBar")
-
         self.progressBar.hide()
-
         spacerItem0 = QtWidgets.QSpacerItem(0.08*int(width.__str__()), 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.midGrid.addItem(spacerItem0, 0, 3, 1, 1)
-
         self.sigLabel = QtWidgets.QLabel(self.centralwidget)
         font = QtGui.QFont()
         font.setFamily("Times New Roman")
         font.setPointSize(20)
         self.sigLabel.setFont(font)
         self.sigLabel.setObjectName("sigLabel")
-
         self.sigLabel.setToolTip("Standard Deviation Of Fiber Distribution")
         self.midGrid.addWidget(self.sigLabel, 0, 10, 1, 1)
 
@@ -77,20 +79,11 @@ class Ui_MainWindow(object):
         self.RLabel.setFont(font)
         self.RLabel.setTextFormat(QtCore.Qt.AutoText)
         self.RLabel.setObjectName("RLabel")
-
-
-        #tip = QtWidgets.QToolTip()
-        #tip.showText("Goodness of Fit")
-        #tip.setFont("Times New Roman")
-        #self.RLabel.setToolTip(tip)
         self.RLabel.setToolTip("Goodness of Fit")
         self.midGrid.addWidget(self.RLabel, 0, 4, 1, 1)
-
         wWidth = 0.04*int(width.__str__())
-
         spacerItem1 = QtWidgets.QSpacerItem(0.04*int(width.__str__()), 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.midGrid.addItem(spacerItem1, 0, 5, 1, 1)
-
         # mu label
         self.muLabel = QtWidgets.QLabel(self.centralwidget)
         font = QtGui.QFont()
@@ -100,10 +93,8 @@ class Ui_MainWindow(object):
         self.muLabel.setObjectName("muLabel")
         self.muLabel.setToolTip("Mean Orientation")
         self.midGrid.addWidget(self.muLabel, 0, 6, 1, 1)
-
         spacerItem2 = QtWidgets.QSpacerItem(wWidth, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.midGrid.addItem(spacerItem2, 0, 7, 1, 1)
-
         #k label
         self.kLabel = QtWidgets.QLabel(self.centralwidget)
         font = QtGui.QFont()
@@ -115,16 +106,8 @@ class Ui_MainWindow(object):
         self.kLabel.setObjectName("kLabel")
         self.kLabel.setToolTip("Fiber Dispersion")
         self.midGrid.addWidget(self.kLabel, 0, 8, 1, 1)
-
         spacerItem3 = QtWidgets.QSpacerItem(wWidth, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.midGrid.addItem(spacerItem3, 0, 9, 1, 1)
-
-
-
-        # spacerItem4 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-  #       self.midGrid.addItem(spacerItem4, 0, 9, 1, 1)
-
-
         self.gridLayout.addLayout(self.midGrid, 1, 0, 1, 1)
         self.figureWidget = QtWidgets.QWidget(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.MinimumExpanding,
@@ -136,69 +119,24 @@ class Ui_MainWindow(object):
         self.figureWidget.setObjectName("figureWidget")
         self.figureLayout = QtWidgets.QGridLayout(self.figureWidget)
         self.figureLayout.setObjectName("figureLayout")
-
         self.gridLayout.addWidget(self.figureWidget, 2, 0, 1, 1)
         self.topGrid = QtWidgets.QGridLayout()
         self.topGrid.setObjectName("topGrid")
         spacerItem3 = QtWidgets.QSpacerItem(wWidth, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.topGrid.addItem(spacerItem3, 0, 6, 1, 1)
-        #self.topGrid.addWidget(self.progressBar, 0, 5, 1, 1)
-
-
-        # self.verticalLayout = QtWidgets.QHBoxLayout()
-        # self.verticalLayout.setObjectName("verticalLayout")
-        # self.verticalLayout.setSizeConstraint(QtWidgets.QLayout.SetFixedSize)
-        # # self.progressBar = QtWidgets.QProgressBar(self.widget)
-        # # self.progressBa.setProperty("value", 24)
-        # # self.progressBar_2.setObjectName("progressBar_2")
-        # self.verticalLayout.addWidget(self.progressBar)
-        # spacerItem3 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
-        # self.verticalLayout.addItem(spacerItem3)
-        # self.topGrid.addLayout(self.verticalLayout, 0, 5, 1, 1)
-
-        # self.widget = QtWidgets.QWidget(self.centralwidget)
-        # self.widget.setObjectName("widget")
-        # self.topGrid = QtWidgets.QGridLayout(self.widget)
-        # self.topGrid.setContentsMargins(0, 0, 0, 0)
-        # self.topGrid.setObjectName("topGrid")
-
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
-
-
-
         self.barWidget = QtWidgets.QWidget()
         self.barWidget.setObjectName("barWidget")
         self.barWidget.setSizePolicy(sizePolicy)
         self.gridPLayout = QtWidgets.QGridLayout(self.barWidget)
         self.gridPLayout.setContentsMargins(0, 0, 0, 0)
         self.gridPLayout.setObjectName("gridLayout")
-        # self.progressBar = QtWidgets.QProgressBar(self.barWidget)
-        # sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
-        # sizePolicy.setHorizontalStretch(0)
-        # sizePolicy.setVerticalStretch(0)
-        # sizePolicy.setHeightForWidth(self.progressBar.sizePolicy().hasHeightForWidth())
-        # self.progressBar.setSizePolicy(sizePolicy)
-        # self.progressBar.setProperty("value", 24)
-        # self.progressBar.setObjectName("progressBar")
         self.gridPLayout.addWidget(self.progressBar, 0, 0, 1, 1)
         self.topGrid.addWidget(self.barWidget, 0, 5, 1, 1)
-
-
         self.clearButton = QtWidgets.QPushButton(self.centralwidget)
-
-
-
-
-
-
-
-
-
-
-
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -209,7 +147,6 @@ class Ui_MainWindow(object):
         self.clearButton.setText("")
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(self.find_data_file('clearButton.png')), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        #icon.addPixmap(QtGui.QPixmap("images/clearButton.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.clearButton.setIcon(icon)
         self.clearButton.setIconSize(QtCore.QSize(40, 40))
         self.clearButton.setObjectName("clearButton")
@@ -226,7 +163,6 @@ class Ui_MainWindow(object):
         self.exportButton.setText("")
         icon1 = QtGui.QIcon()
         icon1.addPixmap(QtGui.QPixmap(self.find_data_file('export.png')), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        #icon1.addPixmap(QtGui.QPixmap("images/export.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.exportButton.setIcon(icon1)
         self.exportButton.setIconSize(QtCore.QSize(50, 50))
         self.exportButton.setObjectName("exportButton")
@@ -244,10 +180,8 @@ class Ui_MainWindow(object):
         self.loadButton.setAcceptDrops(False)
         self.loadButton.setAutoFillBackground(False)
         self.loadButton.setText("")
-
         icon2 = QtGui.QIcon()
         icon2.addPixmap(QtGui.QPixmap(self.find_data_file('open.png')), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        #icon2.addPixmap(QtGui.QPixmap("images/open.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.loadButton.setIcon(icon2)
         self.loadButton.setIconSize(QtCore.QSize(50, 50))
         self.loadButton.setObjectName("loadButton")
@@ -265,13 +199,11 @@ class Ui_MainWindow(object):
         self.startButton.setText("")
         icon3 = QtGui.QIcon()
         icon3.addPixmap(QtGui.QPixmap(self.find_data_file('start-icon.png')), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        #icon3.addPixmap(QtGui.QPixmap("images/start-icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.startButton.setIcon(icon3)
         self.startButton.setIconSize(QtCore.QSize(40, 40))
         self.startButton.setObjectName("startButton")
         self.startButton.setToolTip("Run")
         self.topGrid.addWidget(self.startButton, 0, 1, 1, 1)
-
         self.settingsButton = QtWidgets.QPushButton(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -284,13 +216,11 @@ class Ui_MainWindow(object):
         self.settingsButton.setText("")
         icon4 = QtGui.QIcon()
         icon4.addPixmap(QtGui.QPixmap(self.find_data_file('settings.png')), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        #icon4.addPixmap(QtGui.QPixmap("images/settings.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.settingsButton.setIcon(icon4)
         self.settingsButton.setIconSize(QtCore.QSize(40, 40))
         self.settingsButton.setObjectName("settingsButton")
         self.settingsButton.setToolTip("Settings")
         self.topGrid.addWidget(self.settingsButton, 0, 3, 1, 1)
-
         self.nextButton = QtWidgets.QPushButton(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -331,11 +261,15 @@ class Ui_MainWindow(object):
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
         self.menubar.addAction(self.menuFiberfit.menuAction())
-
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
+        """
+        Sets up appropriate text values for the UI.
+        :param MainWindow: instance of FiberFit.fft_mainWindow
+        :return: none
+        """
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "FiberFit"))
         self.RLabel.setText(_translate("MainWindow", ('R'+u"\u00B2"+" = ")))
@@ -347,6 +281,12 @@ class Ui_MainWindow(object):
         self.sigLabel.setText(_translate("MainWindow", "σ = "))
 
     def find_data_file(self, filename):
+        """
+        Allows for dynamic loading of the picture icons. cx_Freeze (framework I am using to make app executable)
+        will use this to populate all of the buttons that have icons.
+        :param filename: name of the picture w/o a path
+        :return: none
+        """
         if getattr(sys, 'frozen', False):
             datadir = os.path.dirname(sys.executable)
         else:
