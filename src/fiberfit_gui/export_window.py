a9bd1361a93543cd9c074f4e45bf5c4c2247d2c4
atulep <aza.tulepber@gmail.com>
2016-06-19 15:09:26 -0600

    Commented the export_window file.

--- src/fiberfit_gui/export_window.py
+++ src/fiberfit_gui/export_window.py
@@ -1,29 +1,26 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file '/Users/azatulepbergenov/Desktop/export_window.ui'
-#
-# Created: Sat Nov 28 23:29:57 2015
-#      by: PyQt5 UI code generator 5.4
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class Ui_Dialog(object):
+    """
+    Defines the view of the Report dialog, which pops out when the Export button was pressed.
+    """
     def setupUi(self, Dialog, screenDim):
+        """
+        Created by PyQt5 UI code generator 5.4
+        :param Dialog: instance of the report.ReportDialog file
+        :param screenDim: dimensions of a screen
+        :return: none
+        """
         Dialog.setObjectName("Dialog")
-        # Dialog.resize(421, 255)
         Dialog.resize(0.2 * screenDim.width(), 0.2 * screenDim.height())
-        # Dialog.setMinimumSize(QtCore.QSize(0.12 * screenDim.width(), 0.12 * screenDim.height()))
         self.gridLayout = QtWidgets.QGridLayout(Dialog)
         self.gridLayout.setObjectName("gridLayout")
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
-        
         self.label = QtWidgets.QLabel(Dialog)
         self.label.setObjectName("label")
         self.horizontalLayout.addWidget(self.label)
-        
         spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.horizontalLayout.addItem(spacerItem)
         self.checkBox_summary = QtWidgets.QCheckBox(Dialog)
@@ -37,12 +34,9 @@ class Ui_Dialog(object):
         self.gridLayout.addWidget(self.buttonBox, 2, 2, 1, 1)
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
-
         self.radio_none = QtWidgets.QRadioButton(Dialog)
         self.verticalLayout.addWidget(self.radio_none)
-        #  self.radio_none.setChecked(True)
         self.radio_none.setEnabled(True)
-
         self.radio_single = QtWidgets.QRadioButton(Dialog)
         self.radio_single.setObjectName("radio_single")
         self.radio_single.setEnabled(True)
@@ -56,19 +50,21 @@ class Ui_Dialog(object):
         self.radio_append.setChecked(True)
         self.radio_append.setObjectName("radio_append")
         self.verticalLayout.addWidget(self.radio_append)
-
         self.checkBox_summary.setChecked(True)
-
         self.gridLayout.addLayout(self.verticalLayout, 1, 0, 2, 2)
         spacerItem1 = QtWidgets.QSpacerItem(20, 20, QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout.addItem(spacerItem1, 1, 2, 1, 1)
-
         self.retranslateUi(Dialog)
         self.buttonBox.accepted.connect(Dialog.accept)
         self.buttonBox.rejected.connect(Dialog.reject)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
+        """
+        Sets up appropriate text values for the UI.
+        :param Dialog: instnace of the report.ReportDialog file
+        :return: none
+        """
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Export Results"))
         self.label.setText(_translate("Dialog", "Report (PDF)"))
