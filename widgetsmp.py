83f21a4c4a458354f64217d0f747d0ee03272fa6
mprender <meprender@gmail.com>
2021-03-05 23:39:23 -0500

    Add files via upload

--- widgetsmp.py
+++ widgetsmp.py
@@ -4,11 +4,17 @@ import matplotlib.pyplot as plt
 from ipywidgets import *
 from IPython.display import display
 from IPython.html import widgets
+import base64
+res = 'computed results'
 plt.style.use('ggplot')
 from src.fibers_QQQQQQ_invert import fiber_analysis
+from quanfima_torun import fiber_visualization
+import os
+import glob
 #button = widgets.Button(description='My Button')
 #out = widgets.Output()
-
+#import ipyvuetify as v   
+from IPython.display import display, FileLink
 # linking button and function together using a button's method
 #button.on_click(on_button_clicked)
 # displaying button and its output together
@@ -20,7 +26,7 @@ from IPython.display import display, Markdown, clear_output
 from ipywidgets import interact, interact_manual
 def s(_):
     s = widgets.FileUpload(accept='', multiple=True)
-    return output
+    return s.data
 
 def run(_):
 
@@ -63,4 +69,74 @@ def run(_):
     button.on_click(on_button_clicked)
     box=widgets.VBox([text1,text2,text3,text4,button])
     display(box,out)
+def run2(_):
+
+    button = widgets.Button(
+        description='Run Fiber Orientation Visualization',
+        layout={'width': '300px'})
+    out= widgets.Output()
+    def on_button_clicked(_):
+      # "linking function with output"
+        with out:
+              # what happens when we press the button
+            clear_output()
+            print('Program executing. Output in output folder on home page')
+            fiber_visualization()
+   
+    button.on_click(on_button_clicked)
+    box=widgets.VBox([button])
+    display(box,out)
+
+from ipywidgets import HTML
+HTML()
     
+def save(_):
+    filename = 'notebook.tar.gz'
+    b64 = base64.b64encode(res.encode())
+    payload = b64.decode()
+    #BUTTONS
+    html_buttons = '''<html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+    <a download="{filename}" href="notebook.tar.gz" download>
+    <button class="p-Widget jupyter-widgets jupyter-button widget-button mod-warning">Download File</button>
+    </a>
+    </body>
+    </html>
+    '''
+    html_button = html_buttons.format(payload=payload,filename=filename)
+    display(HTML(html_button))
+    out= widgets.Output()   
+
+    
+def clear(_):
+    button = widgets.Button(
+        description='Clear Data',
+        layout={'width': '300px'})
+    out= widgets.Output()
+    def on_button_clicked(_):
+      # "linking function with output"
+        with out:
+              # what happens when we press the button
+            clear_output()
+            print('Program executing. Files in Data and output folder will be cleared')
+            files = glob.glob('../FiberFit/Data/*',recursive=True)
+            for f in files:
+                try:
+                    os.remove(f)
+                except OSError as e:
+                    print("Error: %s : %s" % (f, e.strerror))
+
+            files = glob.glob('../FiberFit/output/*',recursive=True)
+
+            for f in files:
+                try:
+                    os.remove(f)
+                except OSError as e:
+                    print("Error: %s : %s" % (f, e.strerror))
+    button.on_click(on_button_clicked)
+    box=widgets.VBox([button])
+    display(box,out)
+    out= widgets.Output() 
